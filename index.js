efe71496ca99e6df9254a130b678f217f20c0c1b
choiwonseok <cs0328@hanmail.net>
2022-06-22 20:38:20 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://wonseok:lanlanluu@boilerplate.cpjadju.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true//useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDb connected...'))
 .catch(err => console.log(err))
@@ -11,8 +20,24 @@ mongoose.connect('mongodb+srv://wonseok:lanlanluu@boilerplate.cpjadju.mongodb.ne
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! ~~안녕')
 })
+
+app.post('/register', (req, res) => {
+
+
+const user = new User(req.body)
+
+user.save((err, userInfo) => {
+  if(err) return res.json({ success: false, err })
+  return res.status(200).json({
+    success: true
+  })
+})
+
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
